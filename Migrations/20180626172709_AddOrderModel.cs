312e35777c615bbe84c7d7079692d93498aca867
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-06-26 23:10:00 +0500

    enable migrations and add first migration on creating table Orders

---
+++ Migrations/20180626172709_AddOrderModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoreEditor.Migrations
+{
+    public partial class AddOrderModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    ImgFolder = table.Column<string>(nullable: true),
+                    UserName = table.Column<string>(nullable: false),
+                    UserOrganization = table.Column<string>(nullable: false),
+                    UserPhone = table.Column<string>(nullable: false),
+                    UserSurname = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orders");
+        }
+    }
+}
