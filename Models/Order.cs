33351a0f9b7f12d601639923b7088941e085e632
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-06-26 23:08:59 +0500

    add Order model, controller and view folder

---
+++ Models/Order.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CoreEditor.Models
+{
+    public class Order
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Имя")]
+        public string UserName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        public string UserSurname { get; set; }
+
+        [Required]
+        [Display(Name = "Телефон")]
+        public string UserPhone { get; set; }
+
+        [Required]
+        [Display(Name = "Организация")]
+        public string UserOrganization { get; set; }
+
+        public string ImgFolder { get; set; }
+    }
+}
