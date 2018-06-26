a5c9b5d20a05a16fa4bbe6763ad94a4ae5f50206
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-06-26 23:11:10 +0500

    add CoreEditorContext

---
+++ Models/CoreEditorContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+
+namespace CoreEditor.Models
+{
+    public class CoreEditorContext : DbContext
+    {
+        public string ConnectionString { get; set; }
+
+        public CoreEditorContext(DbContextOptions<CoreEditorContext> options, IConfiguration config)
+        : base(options)
+        {
+            ConnectionString = config.GetConnectionString("default");
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public IDbConnection Connect()
+        {
+            var connection = new MySqlConnection(ConnectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
