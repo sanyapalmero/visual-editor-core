4520c35b4cb29c120f914cc81b2d252c3e92d5bf
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-07-19 12:03:22 +0500

    Add Material model

---
+++ Models/Material.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace CoreEditor.Models
+{
+    public class Material
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Название материала")]
+        public string MaterialName { get; set; }
+
+        [Display(Name = "Размер")]
+        public string MaterialSize { get; set; }
+
+        [Display(Name = "Цена")]
+        public string MaterialPrice { get; set; }
+    }
+}
