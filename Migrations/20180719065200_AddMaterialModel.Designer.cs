0d764588f57fb8428fad6367283ad06e93202989
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-07-19 12:02:46 +0500

    Add material model migration

--- Migrations/20180719065200_AddMaterialModel.Designer.cs
+++ Migrations/20180719065200_AddMaterialModel.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace CoreEditor.Migrations
 {
     [DbContext(typeof(CoreEditorContext))]
-    partial class CoreEditorContextModelSnapshot : ModelSnapshot
+    [Migration("20180719065200_AddMaterialModel")]
+    partial class AddMaterialModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180719065200_AddMaterialModel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoreEditor.Migrations
+{
+    public partial class AddMaterialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Material",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    MaterialName = table.Column<string>(nullable: false),
+                    MaterialPrice = table.Column<string>(nullable: true),
+                    MaterialSize = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Material", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Material");
+        }
+    }
+}
