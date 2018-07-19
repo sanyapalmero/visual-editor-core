afb4c80adb4be78908c2739b870ffcb63e0b364f
Alexandr Vagner <alexmednogorsk@mail.ru>
2018-07-20 00:02:16 +0500

    Add migration for creating field status in order model

--- Migrations/20180719090012_AddStatusFieldInOrderModel.Designer.cs
+++ Migrations/20180719090012_AddStatusFieldInOrderModel.Designer.cs
@@ -10,9 +10,10 @@ using System;
 namespace CoreEditor.Migrations
 {
     [DbContext(typeof(CoreEditorContext))]
-    partial class CoreEditorContextModelSnapshot : ModelSnapshot
+    [Migration("20180719090012_AddStatusFieldInOrderModel")]
+    partial class AddStatusFieldInOrderModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180719090012_AddStatusFieldInOrderModel.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace CoreEditor.Migrations
+{
+    public partial class AddStatusFieldInOrderModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaterialSize",
+                table: "Material",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaterialPrice",
+                table: "Material",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaterialSize",
+                table: "Material",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MaterialPrice",
+                table: "Material",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
